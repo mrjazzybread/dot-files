05862fdcf16d1f1426a58953570f0e43db61f826
Tiago Soares <tl.soares@campus.fct.unl.pt>
2025-07-03 13:00:59 +0200

    sync personal files

--- .update.sh
+++ .update.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/sh
 alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
+git --git-dir=$HOME/roam/daily/.git pull
+config pull
 config add -u
 config commit -m "changes"
 config push
