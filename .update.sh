97b385c6ed8ef7b4062238efed6ecb4c8fd43179
Tiago <tl.soares@campus.fct.unl.pt>
2025-10-21 21:11:28 +0100

    Script to update agenda

--- .update.sh
+++ .update.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/sh
-alias config='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 git --git-dir=$HOME/roam/daily/.git pull
-config pull
-config add -u
-config commit -m "changes"
-config push
